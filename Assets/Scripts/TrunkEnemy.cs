9260ed935088126de2fb1f489412ea91c41817e5
Joey Lim <jyorin0206@gmail.com>
2023-01-27 22:04:33 +0800

    Refactored TrunkEnemy to not use Collision Bridge
    - Trunk uses OverlapBox to detect player on top
    - Trunk compares x positions to determine if player collides from the side edges

--- Assets/Scripts/TrunkEnemy.cs
+++ Assets/Scripts/TrunkEnemy.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class TrunkEnemy : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] Transform playerCheck;
+    [SerializeField] LayerMask playerLayerMask;
+
     private Rigidbody2D rb;
+    private BoxCollider2D boxCollider2D;
 
     // use enum to make direction more readable
     private Direction horizontalDirection = Direction.Left;
@@ -14,9 +18,20 @@ public class TrunkEnemy : MonoBehaviour
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        boxCollider2D = gameObject.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
+    private void Update()
+    {
+        if (isPlayerOnTop())
+        {
+            // ensure player bounces before destroying gameObject
+            // set a delay to ensure that player touches the Trunk's collider with Bouncy PhysicsMaterial
+            Destroy(gameObject, 0.1f);
+        }
+    }
+
     void FixedUpdate()
     {
         int horizontalMovement;
@@ -37,36 +52,58 @@ public class TrunkEnemy : MonoBehaviour
         rb.velocity = new Vector2(horizontalMovement * speed, rb.velocity.y);
     }
 
-    /* As some colliders are children of the GameObject,
-     * pass this method to each child to handle their collisions here.
-     * 
-     * ColliderSide will be used to identify which child the collision was from
-     */
-    public void OnChildCollisionDetected(ColliderSide childColliderSide, Collision2D collision)
+    private bool isPlayerOnTop()
     {
-        string colliderTag = collision.collider.tag;
-        if (colliderTag == "Player")
-        {
-            switch (childColliderSide)
-            {
-                // if player touches enemy on top collider, enemy gets destroyed and the player bounces off
-                case ColliderSide.Top:
-                    Destroy(gameObject);
-                    break;
+        bool isOnTop = false;
 
-                // if player touches enemy by the sides, player loses a life
-                case ColliderSide.Side:
+        Collider2D collider = Physics2D.OverlapBox(playerCheck.position, new Vector2(1.5f, 0.1f), 0, playerLayerMask);
+        Vector3 extentsOfTrunk = boxCollider2D.bounds.extents;
+        Vector3 centerOfTrunk = boxCollider2D.bounds.center;
+        Vector3 positionOfIncomingColider = collider.transform.position;
+
+        // if incoming collider touches the top of Trunk and is tagged Player, then player is on top
+        if (positionOfIncomingColider.y >= centerOfTrunk.y + extentsOfTrunk.y && collider.tag == "Player")
+        {
+            isOnTop = true;
+
+        }
+        return isOnTop;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawCube(playerCheck.position,new Vector3(2f,0.1f,0));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        switch (collision.collider.tag)
+        {
+            case "Player":
+                Vector3 extentsOfTrunk = boxCollider2D.bounds.extents;
+                Vector3 centerOfTrunk = boxCollider2D.bounds.center;
+                Vector3 positionOfIncomingColider = collision.collider.transform.position;
+                /* if incoming collider touches the left and right edges of Trunk, player loses
+                 * extents returns half the size of the collider.
+                 * we are checking if player's x point is less than center position - half the length of Trunk
+                 * or if x point is greater than center position + half the length of Trunk
+                 */
+                if (positionOfIncomingColider.x > centerOfTrunk.x + extentsOfTrunk.x ||
+                    positionOfIncomingColider.x < centerOfTrunk.x - extentsOfTrunk.x)
+                {
                     Destroy(collision.gameObject);
                     GameManager.Instance.OnGameLose();
-                    break;
-            }
-        }
-        if (colliderTag == "Ground" || colliderTag == "InstantDeath" || colliderTag == "Ice")
-        {
-            // flip the horizontal direction if Trunk bumps into wall or spikes
-            horizontalDirection = horizontalDirection == Direction.Left ? Direction.Right : Direction.Left;
-        }
-       
 
+                }
+                break;
+
+            case "Wall":
+            case "InstantDeath":
+            case "Ice":
+                // flip the horizontal direction if Trunk bumps into wall or spikes
+                horizontalDirection = horizontalDirection == Direction.Left ? Direction.Right : Direction.Left;
+                break;
+        }
     }
 }
