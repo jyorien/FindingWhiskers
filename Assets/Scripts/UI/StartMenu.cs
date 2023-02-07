e511150b82676312167023269904fea970850bc0
Joey Lim <jyorin0206@gmail.com>
2023-02-08 00:13:23 +0800

    Fixed start menu level start bug

--- Assets/Scripts/UI/StartMenu.cs
+++ Assets/Scripts/UI/StartMenu.cs
@@ -11,7 +11,7 @@ public class StartMenu : MonoBehaviour
     {
         // start the game
         startButton.onClick.AddListener(() => {
-            GameManager.Instance.GoNextLevel();
+            GameManager.Instance.GoToLevel(1);
         });
     }
 }
